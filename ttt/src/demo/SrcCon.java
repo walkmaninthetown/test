91f88eab447a9cfd5c2e8605af2b1e9e59ce7b19
lve <2662509645@qq.com>
2023-10-09 18:39:53 +0800

    hello world!

---
+++ ttt/src/demo/SrcCon.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author xiaoming xxx@163.com
+ * @version 2023/10/9 18:36
+ * @since JDK8
+ */
+public class SrcCon {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
